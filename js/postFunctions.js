24c78774f1ab86d902396dc49c99076507f2fbb8
dani ferdinan <daniferdinandall@gmail.com>
2023-10-25 17:38:13 +0700

    udpate

--- js/postFunctions.js
+++ js/postFunctions.js
@@ -2,8 +2,7 @@ import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
 // import { setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
 export default function PostSignUp() {
-    let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post";
-    // let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/trensentimen-post2";
+    let target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/post-login";
     let tokenkey = "token";
     let tokenvalue = "f48bd58cb3b3972d05bb9303b15ce9b83f4fcb9c871d1b05906f2fec20620ea0";
     let datainjson = {
