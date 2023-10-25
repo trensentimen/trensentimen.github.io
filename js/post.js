c2470c7f371bd9acd23731138601569ef5402b49
dani ferdinan <daniferdinandall@gmail.com>
2023-10-25 16:03:17 +0700

    first commit

---
+++ js/post.js
@@ -0,0 +1,5 @@
+// import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+// import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+import PostSignUp from "./postFunctions.js";
+
+window.PostSignUp = PostSignUp;
