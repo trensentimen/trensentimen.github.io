a99125ffe5000dd7ad794cf46e46cd8c836faeae
dani ferdinan <daniferdinandall@gmail.com>
2023-10-26 15:28:13 +0700

    update

--- js/post.js
+++ js/post.js
@@ -1,5 +1,6 @@
 // import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 // import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
-import PostSignUp from "./postFunctions.js";
+// import PostSignUp from "./postFunctions.js";;
+import PostSignUp from "https://cdn.jsdelivr.net/gh/trensentimen/js@main/postLogin.js";
 
 window.PostSignUp = PostSignUp;
