e8d3372b0d46873b6bdeba0b65781f6cc730f885
dani ferdinan <daniferdinandall@gmail.com>
2023-11-05 12:36:39 +0700

    fix signup

--- js/signup.js
+++ js/signup.js
@@ -2,7 +2,7 @@ import { post } from "./utilities/api.js";
 import { getValue } from "https://jscroot.github.io/element/croot.js";
 
 const PostSignUp = () => {
-    const target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/login";
+    const target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/register";
     const datainjson = {
         email: getValue("email"),
         password: getValue("password")
