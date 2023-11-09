b5e772b0164fce59bd6797fa443307ffa549ff23
dani ferdinan <daniferdinandall@gmail.com>
2023-11-09 16:10:04 +0700

    update deletetopic

---
+++ js/deleteData.js
@@ -0,0 +1,47 @@
+import { postWithBearer } from "./utilities/api.js";
+// import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+import getCookie from "./getCookie.js";
+import { addInner } from "./utilities/element.js"
+
+const deleteData = () => {
+    if(!confirm("Apakah anda yakin ingin menghapus topik ini?"))
+    {
+        return;
+    }
+
+    const target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/deleteTopic";
+
+    let id = false;
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('id')) {
+        if (urlParams.get('id') != '') {
+            id = urlParams.get('id');
+        }
+    }
+    const datainjson = {
+        "_id": id
+    };
+    const token = getCookie("token")
+    if (token) {
+        postWithBearer(target_url, token, datainjson, responseData)
+    } else {
+        console.log("token tidak ada " + result.message);
+        alert("sesi anda sudah habis, silahkan logout dan login ulang")
+        window.location.href = "sentimen.html";
+    }
+    // post(target_url, datainjson, responseData);
+};
+
+const responseData = (result) => {
+    // console.log(result);
+    if (result.status === true) {
+        alert("Berhasil menghapus topik")
+        window.location.href = "sentimen.html";
+    } else {
+        console.log(result.message);
+        alert(`parameter bermasalah atau sesi anda sudah habis, silahkan ulangi atau logout dan login ulang`);
+        window.location.href = "sentimen.html";
+    }
+};
+
+window.deleteData = deleteData;
