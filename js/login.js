f4d9bdd73c6dfeeca98ba65f68b21eb3f0b43f3d
dani ferdinan <daniferdinandall@gmail.com>
2023-11-12 14:48:28 +0700

    udpate loading

--- js/login.js
+++ js/login.js
@@ -1,7 +1,9 @@
 import { post } from "./utilities/api.js";
 import { setInner, getValue } from "https://jscroot.github.io/element/croot.js";
+import {showLoadingModal, hideLoadingModal} from "./utilities/loading.js"
 
 const PostSignIn = () => {
+    showLoadingModal
     const target_url = "https://asia-southeast2-trens-project.cloudfunctions.net/login";
     const datainjson = {
         email: getValue("email"),
@@ -15,9 +17,11 @@ const responseData = (result) => {
     // console.log(result);
     if (result.status === true) {
         setCookieWithExpireHour("token", result.token, 2);
+        hideLoadingModal
         alert(`Berhasil Masuk ${result.message}`);
         window.location.href = "dashboard.html";
     } else {
+        hideLoadingModal()
         alert(`Gagal Masuk password atau email salah`);
     }
 };
